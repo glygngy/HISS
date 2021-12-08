c452126d9430b99fe82bf47e894cdb9d7876844e
aliteptep <94163316+aliteptep@users.noreply.github.com>
2021-12-07 18:05:04 -1200

    Add files via upload

---
+++ pregister.php
@@ -0,0 +1,68 @@
+<?php include('pserver.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Register</h2>
+  </div>
+	
+  <form method="post" action="register.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+		<label>Full name</label>
+		<input type="Full name" name="Full name" value="">
+	</div>
+	<div class="input-group">
+  	  <label>Age</label>
+  	  <input type="Age" name="Age" value=" ">
+	  
+              </div>
+              <div class="form-group">
+                <label for="gender">Gender</label>
+                <div>
+                  <label for="male" class="radio-inline"
+                    ><input
+                      type="radio"
+                      name="gender"
+                      value="m"
+                      id="male"
+                    />Male</label
+                  >
+                  <label for="female" class="radio-inline"
+                    ><input
+                      type="radio"
+                      name="gender"
+                      value="f"
+                      id="female"
+                    />Female</label
+                  >
+	<div class="input-group">
+		<label>Address</label>
+		<input type="text" name="Address" value="">
+	</div>
+	<div class="input-group">
+  	  <label>Phone</label>
+  	  <input type="Phone" name="Phone" value=" ">
+  	</div>
+  	</div>
+  	<div class="input-group">
+  	  <label>Email</label>
+  	  <input type="email" name="email" value="<?php echo $email; ?>">
+  	</div>
+  	<div class="input-group">
+  	  <label>Password</label>
+  	  <input type="password" name="password_1">
+  	</div>
+  	<div class="input-group">
+  	  <label>Confirm password</label>
+  	  <input type="password" name="password_2">
+  	</div>
+  	<div class="input-group">
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+  	</div>
+  	<p>
+  		Already a member? <a href="login.php">Sign in</a>

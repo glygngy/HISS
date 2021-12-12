789e0e14d244982714c820714fc543fb72e6066d
Coldlane013 <lalaine.costales@gmail.com>
2021-12-12 19:36:56 +0800

    ...

--- application/controllers/Calendar.php
+++ application/controllers/Calendar.php
@@ -1,5 +1,15 @@
 <?php	
 class Calendar extends CI_Controller{
+public function __construct(){
+		parent::__construct();
+		$this->load->model('calendar_model');
+		date_default_timezone_set('Asia/Manila');
+	}
+	
+	public function display($year=NULL,$month=NULL){
+        $this->load->helper('url');
+		$this->load->view("calendar/display",$data);
 
+	}
 }
 ?>
--- application/views/calendar/display_sched.php
+++ application/views/calendar/display_sched.php
@@ -1,7 +1,2 @@
 <?php $this->load->view('header', array('num' => 1, 'title' => 'Clinic Calendar')); ?>
-<?php 
-	$dates=$date["closed"];
-	$ctr=$date["ctr"];
-	$i;
-;?>
 <h1> test</h1>

980c4ef8f5b43755d683fe3b3ced35fef53bc09f
Coldlane013 <lalaine.costales@gmail.com>
2021-12-12 17:43:26 +0800

    add note

--- application/views/main/home.php
+++ application/views/main/home.php
@@ -1,6 +1,6 @@
 <?php $this->load->view('header', array('num' => 0, 'title' => 'HOME')); ?>
                 <h3 class="text-center"><strong>IM THE HOME PAGE PLEASE DESIGN ME <br>  
-                    <a class="create-app" href="<?php echo base_url();?>calendar/display">Book for an appointment</a> </p>
+                    <a class="create-app" href="<?php echo base_url();?>calendar/display">Book for an appointment(connect me to patient login)</a> </p>
               </div>
             </div>
 <?php $this->load->view('footer'); ?>
